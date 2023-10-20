f0cd2d707108e61dfd14a470c0e49dfdb7abd97a
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-20 15:37:21 -0300

    20/10

--- SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
+++ SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
 export default {
-  components: {
-    Home,
-  },
+  name: "App",
 };
 </script>
