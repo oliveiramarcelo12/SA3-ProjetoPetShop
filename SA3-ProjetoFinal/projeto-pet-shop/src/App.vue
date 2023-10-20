dc5730be69fc66640594737b989b8fb757b05748
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-20 14:29:24 -0300

    20/10

--- SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
+++ SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <Header />
     <Carousel />
-    <ProductContent />
+    < ProductList />
     <Footer />
   </div>
 </template>
@@ -10,14 +10,14 @@
 <script>
 import Header from "@/components/Header.vue";
 import Carousel from "@/components/Carousel.vue";
-import ProductContent from "@/components/ProductContent.vue";
+import  ProductList from "@/components/ ProductList.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     Header,
     Carousel,
-    ProductContent,
+     ProductList,
     Footer,
   },
 };
