affad9530ca3bd169ebb84ba162ffcac187654d7
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-20 15:28:27 -0300

    20/10

--- SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
+++ SA3-ProjetoFinal/projeto-pet-shop/src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div id="app">
     <Home />
-     <Racao />
-     <Acessorios />
   </div>
 </template>
 
 <script>
 import Home from "@/components/Home.vue";
-import Racao from "@/components/Racao.vue";
-import Acessorios from "@/components/Acessorios.vue";
 export default {
   components: {
     Home,
-     Racao,
-    Acessorios,
-   
   },
 };
 </script>
