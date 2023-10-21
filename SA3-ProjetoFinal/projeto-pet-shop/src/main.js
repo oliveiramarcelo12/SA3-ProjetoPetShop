a1c227c2f2ecfdf8e28de8ad4b93e35ff9287525
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-10-20 21:49:32 -0300

    20/10

--- SA3-ProjetoFinal/projeto-pet-shop/src/main.js
+++ SA3-ProjetoFinal/projeto-pet-shop/src/main.js
@@ -1,12 +1,24 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Importe o arquivo router.js
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import "bootstrap/dist/css/bootstrap.css"; // Importe o CSS do Bootstrap
+import "bootstrap/dist/js/bootstrap.js"; // Importe o JavaScript do Bootstrap
 
-const app = createApp(App);
-app.use(router); // Integre o roteador no aplicativo
-app.use(BootstrapVue);
+import HomePage from "@/components/HomePage.vue";
+import RacaoPage from "@/components/RacaoPage.vue";
+import AcessoriosPage from "@/components/AcessoriosPage.vue";
+Vue.use(VueRouter);
 
-app.mount('#app');
+const router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage }, // Rota para a página inicial
+      { path: "/racao", component: RacaoPage }, // Adicione a rota para a página "racao"
+     { path: "/acessorios", component: AcessoriosPage }, // Adicione a rota para a página "acessorios"
+  ],
+});
+
+new Vue({
+  el: "#app",
+  router,
+  render: (h) => h(App),
+});
