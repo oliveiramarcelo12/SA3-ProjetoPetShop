bb0c88a08e13f318c5e2eff9d208fbda1d73d3f0
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-24 09:49:52 -0300

    24/10

--- SA3-ProjetoFinal/projeto-pet-shop-html/cadastro.js
+++ SA3-ProjetoFinal/projeto-pet-shop-html/cadastro.js
@@ -1,1 +1,27 @@
+// cadastro.js
+
+// Função para validar o formulário de contato
+function validarFormulario() {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var assunto = document.getElementById("assunto").value;
+    var mensagem = document.getElementById("exampleFormControlTextarea1").value;
+
+    if (nome === "" || email === "" || assunto === "" || mensagem === "") {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+        return false;
+    }
+
+    return true;
+}
+
+// Event listener para o envio do formulário
+var formulario = document.getElementById("formularioContato");
+if (formulario) {
+    formulario.addEventListener("submit", function (e) {
+        if (!validarFormulario()) {
+            e.preventDefault(); // Impede o envio do formulário se a validação falhar
+        }
+    });
+}
 
