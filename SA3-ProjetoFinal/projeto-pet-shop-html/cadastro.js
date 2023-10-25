aea76e6dd3c33958166f35b663888841b35068ad
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-25 09:42:46 -0300

    25/10

--- SA3-ProjetoFinal/projeto-pet-shop-html/cadastro.js
+++ SA3-ProjetoFinal/projeto-pet-shop-html/cadastro.js
@@ -1,27 +1,40 @@
 // cadastro.js
 
-// Função para validar o formulário de contato
-function validarFormulario() {
-    var nome = document.getElementById("nome").value;
+// Função para validar o formulário de cadastro
+function validarFormularioCadastro() {
     var email = document.getElementById("email").value;
-    var assunto = document.getElementById("assunto").value;
-    var mensagem = document.getElementById("exampleFormControlTextarea1").value;
+    var confirmEmail = document.getElementById("confirmEmail").value;
+    var senha = document.getElementById("senha").value;
+    var confirmSenha = document.getElementById("confirmSenha").value;
+    var endereco = document.getElementById("endereco").value;
+    var cep = document.getElementById("cep").value;
+    var estado = document.getElementById("estado").value;
+    var cidade = document.getElementById("cidade").value;
 
-    if (nome === "" || email === "" || assunto === "" || mensagem === "") {
+    if (email === "" || confirmEmail === "" || senha === "" || confirmSenha === "" || endereco === "" || cep === "" || estado === "" || cidade === "") {
         alert("Por favor, preencha todos os campos obrigatórios.");
+        return false;
+    }
+
+    if (email !== confirmEmail) {
+        alert("Os campos de email e confirmar email devem corresponder.");
+        return false;
+    }
+
+    if (senha !== confirmSenha) {
+        alert("Os campos de senha e confirmar senha devem corresponder.");
         return false;
     }
 
     return true;
 }
 
-// Event listener para o envio do formulário
-var formulario = document.getElementById("formularioCadastro");
-if (formulario) {
-    formulario.addEventListener("submit", function (e) {
-        if (!validarFormulario()) {
+// Event listener para o envio do formulário de cadastro
+var formularioCadastro = document.getElementById("formularioCadastro");
+if (formularioCadastro) {
+    formularioCadastro.addEventListener("submit", function (e) {
+        if (!validarFormularioCadastro()) {
             e.preventDefault(); // Impede o envio do formulário se a validação falhar
         }
     });
 }
-
